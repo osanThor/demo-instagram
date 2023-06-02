ee2d05d67551405f3292f26a244e2c9e16db2d7c
junyoung <jadw9715@gmail.com>
2023-06-03 01:04:11 +0900

    feat. navbar layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+import Link from "next/link";
+import HomeFillIcon from "./ui/icons/HomeFillIcon";
+import HomeIcon from "./ui/icons/HomeIcon";
+import NewFillIcon from "./ui/icons/NewFillIcon";
+import NewIcon from "./ui/icons/NewIcon";
+import SearchFillIcon from "./ui/icons/SearchFillIcon";
+import SearchIcon from "./ui/icons/SearchIcon";
+import { usePathname } from "next/navigation";
+
+const menu = [
+  { href: "/", icon: <HomeIcon />, clickedIcon: <HomeFillIcon /> },
+  { href: "/search", icon: <SearchIcon />, clickedIcon: <SearchFillIcon /> },
+  { href: "/new", icon: <NewIcon />, clickedIcon: <NewFillIcon /> },
+];
+
+export default function Navbar() {
+  const pathName = usePathname();
+  return (
+    <div>
+      <Link href="/">
+        <h1>Instagram</h1>
+      </Link>
+      <nav>
+        <ul>
+          {menu.map((item) => (
+            <li key={item.href}>
+              <Link href={item.href}>
+                {pathName === item.href ? item.clickedIcon : item.icon}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+}
