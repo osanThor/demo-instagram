781cdd062521466afcb608504e513841f1385eb7
junyoung <jadw9715@gmail.com>
2023-06-06 21:47:33 +0900

    feat. login 시 sanity user 추가

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -4,7 +4,7 @@ export default function Avatar({ image }: Props) {
     <div className="rounded-full w-9 h-9 bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300">
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        className="rounded-full p-[0.1rem]"
+        className="rounded-full p-[0.15rem]"
         src={image ?? undefined}
         alt="user profile"
         referrerPolicy="no-referrer"
