10a9c5fa3fb57b03c86b8208e29abecc97a63f20
junyoung <jadw9715@gmail.com>
2023-06-08 00:05:33 +0900

    feat. sanity api query GROQ

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -1,4 +1,5 @@
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getUserByUsername } from "@/service/user";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
@@ -9,5 +10,7 @@ export async function GET(req: Request) {
   if (!user) {
     return new Response("Authentication Error", { status: 401 });
   }
-  return NextResponse.json("hello");
+  return getUserByUsername(user.username).then((data) =>
+    NextResponse.json(data)
+  );
 }
