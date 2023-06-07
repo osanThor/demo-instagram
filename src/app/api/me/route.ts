93e34790bcbce411f733b21b7e0e007ce5c95433
junyoung <jadw9715@gmail.com>
2023-06-07 23:43:30 +0900

    feat. api user info api

---
+++ src/app/api/me/route.ts
@@ -0,0 +1,13 @@
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    return new Response("Authentication Error", { status: 401 });
+  }
+  return NextResponse.json("hello");
+}
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,13 +2,11 @@ import { addUser } from "@/service/user";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_OAUTH_ID || "",
       clientSecret: process.env.GOOGLE_OAUTH_SECRET || "",
     }),
-    // ...add more providers here
   ],
   callbacks: {
     async signIn({ user: { id, name, image, email } }) {
