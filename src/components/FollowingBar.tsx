9c52a97f274c8e9828ccb9c4b95e65db9c8a5ed7
junyoung <jadw9715@gmail.com>
2023-06-08 00:19:19 +0900

    feat. sanity following info

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,10 +1,33 @@
 "use client";
-
+import { DetailUser } from "@/model/user";
+import Link from "next/link";
+import { PropagateLoader } from "react-spinners";
 import useSWR from "swr";
+import Avatar from "./Avatar";
 
 export default function FollowingBar() {
-  const { data, isLoading, error } = useSWR("/api/me");
-  // 4. 여기에서, 클라이언트 컴포넌트에서 followings의 정보를 UI에 보여줌
+  const { data, isLoading: loading, error } = useSWR<DetailUser>("/api/me");
+  const users = data?.following;
 
-  return <div>FollowingBar</div>;
+  return (
+    <section>
+      {loading ? (
+        <PropagateLoader size={8} color="red" />
+      ) : (
+        (!users || users.length === 0) && <p>{`Tou don't have Followings`}</p>
+      )}
+      {users && users.length > 0 && (
+        <ul>
+          {users.map(({ username, image }) => (
+            <li key={username}>
+              <Link href={`/user/${username}`}>
+                <Avatar image={image} highlight />
+                <p>{username}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 }
