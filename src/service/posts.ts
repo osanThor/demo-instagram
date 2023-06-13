d49b351566c79c9e759cd64e01da24e5e7444518
junyoung <jadw9715@gmail.com>
2023-06-13 23:31:45 +0900

    Fix: image url function

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,4 +1,5 @@
-import { client } from "./sanity";
+import { SimplePost } from "@/model/post";
+import { client, urlFor } from "./sanity";
 const simplePostProjection = `
     ...,
     "username": author->username,
@@ -11,9 +12,13 @@ const simplePostProjection = `
     "createAt":_createAt
 `;
 export async function getFollowingPostsOf(username: string) {
-  return client.fetch(
-    `*[_type == "post" && author->username == "${username}"
+  return client
+    .fetch(
+      `*[_type == "post" && author->username == "${username}"
           || author._ref in *[_type == "user" && username == "${username}"].following[]._ref]
           | order(_createAt desc){${simplePostProjection}}`
-  );
+    )
+    .then((posts) =>
+      posts.map((post: SimplePost) => ({ ...post, image: urlFor(post.image) }))
+    );
 }
