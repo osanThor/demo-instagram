962a9c7b0d0b60ab9b7f36aa35bf3face488aac4
junyoung <jadw9715@gmail.com>
2023-06-21 22:43:46 +0900

    style. profile and Refactor. avatat size func

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -13,19 +13,22 @@ export default function UserProfile({ user }: Props) {
     { title: "following", number: following },
   ];
   return (
-    <section>
-      <Avatar image={image} highlight />
-      <div>
-        <h1>{username}</h1>
-        <FollowButton user={user} />
-        <ul>
+    <section className="flex flex-col items-center justify-center w-full py-12 border-b md:flex-row border-neutral-300">
+      <Avatar image={image} highlight size="xlarge" />
+      <div className="md:ml-10 basis-1/3">
+        <div className="flex flex-col items-center md:flex-row">
+          <h1 className="my-2 text-2xl md:mr-8 md:mb-0">{username}</h1>
+          <FollowButton user={user} />
+        </div>
+        <ul className="flex gap-4 my-4">
           {info.map(({ title, data }, idx) => (
             <li key={idx}>
-              <span>{data}</span>
+              <span className="mr-1 font-bold">{data}</span>
               {title}
             </li>
           ))}
         </ul>
+        <p className="text-xl font-bold text-center md:text-start">{name}</p>
       </div>
     </section>
   );
