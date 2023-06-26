08667d319f502f51e9cdb34ac14ff74555537d25
junyoung <jadw9715@gmail.com>
2023-06-26 22:47:50 +0900

    Feat. post detail comment hook

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -5,18 +5,20 @@ import PostUserAvatar from "./PostUserAvatar";
 import ActionBar from "./ActionBar";
 import CommentForm from "./CommentForm";
 import Avatar from "./Avatar";
-import usePosts from "@/hooks/posts";
+import useFullPost from "@/hooks/post";
+import useMe from "@/hooks/me";
 
 type Props = {
   post: SimplePost;
 };
 export default function PostDetail({ post }: Props) {
   const { id, userImage, username, image, text } = post;
-  const { data } = useSWR<FullPost>(`/api/posts/${id}`);
+  const { post: data, postComment } = useFullPost(id);
+  const { user } = useMe();
   const comments = data?.comments;
-  const { postComment } = usePosts();
   const handlePostComment = (comment: string) => {
-    postComment(post, comment);
+    user &&
+      postComment({ comment, username: user.username, image: user.image });
   };
   return (
     <section className="flex w-full h-full">
