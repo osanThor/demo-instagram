7e53acce66c78f207f58f51236b052444f37b832
junyoung <jadw9715@gmail.com>
2023-07-19 00:38:48 +0900

    feat. drag and drop

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,18 @@
-export default function NewPostPage() {
-  return <p>NewPostPage</p>;
+import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { signIn } from "next-auth/react";
+import NewPost from "@/components/NewPost";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+export const metadata: Metadata = {
+  title: "New Post",
+  description: "Create at New Post",
+};
+export default async function NewPostPage() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/auth/signin");
+  }
+  return <NewPost user={session.user} />;
 }
