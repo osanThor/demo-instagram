aa76e5091993da03aeab6a51c47d09035bc56f76
junyoung <jadw9715@gmail.com>
2023-08-02 23:42:30 +0900

    Refact. API Router Session

--- src/app/api/follow/route.ts
+++ src/app/api/follow/route.ts
@@ -1,22 +1,17 @@
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { follow, unfollow } from "@/service/user";
-import { getServerSession } from "next-auth";
+import { withSessionUser } from "@/utils/session";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(req: NextRequest) {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
+  return withSessionUser(async (user) => {
+    const { id: targetId, follow: isFollow } = await req.json();
+    if (!targetId || isFollow == null) {
+      return new Response("Bad Request", { status: 400 });
+    }
 
-  if (!user) {
-    return new Response("Authentication Error", { status: 401 });
-  }
-  const { id: targetId, follow: isFollow } = await req.json();
-  if (!targetId || isFollow === undefined) {
-    return new Response("Bad Request", { status: 400 });
-  }
-
-  const request = isFollow ? follow : unfollow;
-  return request(user.id, targetId)
-    .then((res) => NextResponse.json(res))
-    .catch((error) => new Response(JSON.stringify(error), { status: 500 }));
+    const request = isFollow ? follow : unfollow;
+    return request(user.id, targetId)
+      .then((res) => NextResponse.json(res))
+      .catch((error) => new Response(JSON.stringify(error), { status: 500 }));
+  });
 }
